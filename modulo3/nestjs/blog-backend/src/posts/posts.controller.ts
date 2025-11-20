cd770d30e6c156304cf28a35a641bfbeef7409b8
User <gadieldylan02@gmail.com>
2025-11-20 10:07:08 -0500

    Clase 2025-11-20

--- modulo3/nestjs/blog-backend/src/posts/posts.controller.ts
+++ modulo3/nestjs/blog-backend/src/posts/posts.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Post as HttpPost, Body, Get, Param, Put, Delete,UseGuards } from '@nestjs/common';
+import { Controller, Post as HttpPost, Body, Get, Param, Put, Delete,UseGuards, Query} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Post } from './post.entity';
+import { Pagination } from 'nestjs-typeorm-paginate';
+
 
 @Controller('posts')
 export class PostsController {
@@ -15,8 +18,12 @@ export class PostsController {
   }
 
   @Get()
-  findAll() {
-    return this.postsService.findAll();
+       findAll(
+         @Query('page') page = 1,
+         @Query('limit') limit = 10,
+       ): Promise<Pagination<Post>> {
+         limit = limit > 100 ? 100 : limit;
+         return this.postsService.findAll({ page, limit });
   }
 
   @Get(':id')
